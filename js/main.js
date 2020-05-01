8001304b4d72ddde90e68c4a55768ab516d7cb01
zabit-musalaev <zabit.mv@gmail.com>
2020-05-02 01:17:35 +0300

    responsive all pages

--- js/main.js
+++ js/main.js
@@ -23,6 +23,28 @@ $(document).ready(function () {
 
   new WOW().init();
 
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+      var modalDialog = $('.modal__dialog');
+
+      modal.on('click', function(e){
+        if (e.target != modalDialog[0] && modalDialog.has(e.target).length === 0) {
+          modal.removeClass('modal--visible');
+        }
+      })
+
+      modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+      });
+
+      closeBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+
+  });
+
+  $('[type=tel]').mask('+7(999) 999-99-99', {placeholder: "Ваш номер телефона"});
+
 }); 
 
   
